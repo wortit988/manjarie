2a34757f45c7cdfa50d4530798d97ece8fa10b1e
wortit988 <106458453+wortit988@users.noreply.github.com>
2024-08-18 22:32:58 +0530

    Removed unused Import

--- src/components/CartSummary.js
+++ src/components/CartSummary.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import Modal from "./modal/Modal";
-import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { clearCart } from "../utils/cartSlice";
 
