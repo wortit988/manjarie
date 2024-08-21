144e009bdc0324f631798bf5c2c9d42492e19212
wortit988 <106458453+wortit988@users.noreply.github.com>
2024-08-21 22:43:35 +0530

    Added shimmer effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ItemCard from "./ItemCard";
 import { itemList } from "../assets/itemList";
-import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const Body = () => {
